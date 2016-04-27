4dc7ab772eec05184f89e08fdcbc3190f639d434
tdquang <dangquang2011@gmail.com>
2016-04-27 15:42:42 -0500

    Trying to fix semaphore

--- condition.c
+++ condition.c
@@ -6,21 +6,17 @@
 #include <stdbool.h>
 
 
+void* child(void*);
+void* adult(void*);
+void initSynch(void);
 
 pthread_mutex_t loadBoat;
-
-
-pthread_cond_t boatFull;
-pthread_cond_t halfFull;
-pthread_cond_t boatEmpty;
-
-
-
-
 pthread_cond_t boatInOahu;
 pthread_cond_t boatInMol;
+pthread_cond_t lastAdultCrossed;
 pthread_cond_t showedUpInOahu;
 pthread_cond_t finishedTransporting;
+pthread_cond_t transportingChildren;
 
 pthread_mutex_t numChildrenInBoatLock;
 
@@ -32,7 +28,7 @@ pthread_mutex_t inMolAdultLock;
 int childrenInOahu = 0;
 int adultsInOahu = 0;
 int childrenInMol = 0;
-int adultInMol = 0;
+int adultsInMol = 0;
 
 int numChildrenInBoat = 0;
 
@@ -43,28 +39,50 @@ int main(int args, char *argv[]){
 	const int numadults = atoi(argv[2]);
 	pthread_t childthreads[numchildren];
 	pthread_t adultthreads[numadults];
+
+	numpeople = sem_open("num_people", O_CREAT|O_EXCL, 0466, 0);
+  	while (numpeople==SEM_FAILED) {
+	    if (errno == EEXIST) {
+	      printf("semaphore numpeople already exists, unlinking and reopening\n");
+	      fflush(stdout);
+	      sem_unlink("num_people");
+	      numpeople = sem_open("num_people", O_CREAT|O_EXCL, 0466, 10);
+	    }
+	    else {
+	      printf("semaphore could not be opened, error # %d\n", errno);
+	      fflush(stdout);
+	      exit(1);
+	    }
+  	}
+
+  	
+
 	initSynch();
 	
-
+	int err;
 	for (int i = 0; i < numchildren; i++){
 		pthread_create(&childthreads[i], NULL, child, NULL);
-		sem_wait(numpeople);
-
+		printf("Waddup\n");
+		fflush(stdout);
+		err = sem_wait(numpeople);
 	}
 
 	for (int i = 0; i < numadults; i++){
 		pthread_create(&adultthreads[i], NULL, adult, NULL);
-		sem_wait(numpeople);
+		err = sem_wait(numpeople);
 	}
   	
-  	
+  	printf("Here\n");
+	fflush(stdout);
 
-  	pthread_cond_broadcast(&showedUpInOahu, NULL);
+  	pthread_cond_broadcast(&showedUpInOahu);
 
 
 
 
   	pthread_cond_wait(&finishedTransporting, NULL);
+  	sem_close(numpeople);
+  	sem_unlink("num_people");
   	exit(1);
 
 }
@@ -72,12 +90,14 @@ int main(int args, char *argv[]){
 void* child(void* args){
 	printf("Child %s arrived to OAHU\n", (char*) args);
 	fflush(stdout);
-	sem_post(numpeople);
-	bool inOahu = 1;
+	sem_post(numpeople);	
+	bool inOahu = true;
 	pthread_mutex_lock(&inOahuChildLock);
 	childrenInOahu++;
 	pthread_mutex_unlock(&inOahuChildLock);
 	pthread_cond_wait(&showedUpInOahu, NULL);
+	printf("Everyone has arrived");
+	fflush(stdout);
 	
 	while (1){
 		if (inOahu){
@@ -86,27 +106,44 @@ void* child(void* args){
 				pthread_mutex_unlock(&inOahuChildLock);
 				pthread_mutex_lock(&loadBoat);
 				pthread_cond_wait(&boatInOahu, &loadBoat);
-				pthread_mutex_lock(&numChildrenInBoatLock)
+				pthread_mutex_lock(&numChildrenInBoatLock);
 				numChildrenInBoat++;
 				if (numChildrenInBoat == 1){
+					printf("Child %s boarded boat in OAHU\n", (char*) args);
+					fflush(stdout);
 					pthread_mutex_unlock(&loadBoat);
 				}
 				else{
+					printf("Child %s boarded boat in OAHU\n", (char*) args);
+					fflush(stdout);
 					pthread_mutex_lock(&inOahuChildLock);
 					pthread_mutex_lock(&inMolChildLock);
 					childrenInOahu -= 2;
 					childrenInMol += 2;
 					numChildrenInBoat = 0;
+
 				}
 				inOahu = false;
-				pthread_mutex_unlock(&numChildrenInBoatLock)
+				pthread_mutex_unlock(&numChildrenInBoatLock);
+				printf("Child %s arrived in MOL\n", (char*) args);
+				fflush(stdout);
 				pthread_cond_signal(&boatInMol);
+
 			}
 			else{
 				pthread_mutex_unlock(&inOahuChildLock);
+				pthread_cond_broadcast(&transportingChildren);
 				pthread_mutex_lock(&inOahuAdultLock);
-				if (adultsInOahu > 0){
-					
+				if (adultsInOahu == 0){
+					pthread_mutex_unlock(&inOahuAdultLock);
+					pthread_cond_wait(&boatInOahu, NULL);
+					pthread_mutex_lock(&loadBoat);
+					inOahu = false;
+					childrenInMol++;
+					childrenInOahu--;
+					pthread_cond_signal(&boatInMol);
+					pthread_cond_signal(&finishedTransporting);
+					pthread_mutex_unlock(&loadBoat);
 				}
 			}
 		}
@@ -117,7 +154,11 @@ void* child(void* args){
 			childrenInOahu++;
 			childrenInMol--;
 			inOahu = true;
+			printf("Child %s arrived in OAHU\n", (char*) args);
+			fflush(stdout);
 			pthread_cond_signal(&boatInOahu);
+			pthread_mutex_unlock(&inOahuChildLock);
+			pthread_mutex_unlock(&inMolChildLock);
 		}
 		
 	}
@@ -129,39 +170,41 @@ void* adult(void* args){
 	printf("Adult %s arrived to OAHU\n", (char*) args);
 	fflush(stdout);
 	sem_post(numpeople);
+	int val = 0;
 	pthread_cond_wait(&showedUpInOahu, NULL);
-	bool inOahu = 1;
+	printf("Everyone has arrived");
+	fflush(stdout);
+	bool inOahu = true;
 	pthread_mutex_lock(&inOahuAdultLock);
 	adultsInOahu++;
 	pthread_mutex_unlock(&inOahuAdultLock);
 	pthread_cond_wait(&transportingChildren, NULL);
-	
+	pthread_cond_wait(&boatInOahu, NULL);
+	pthread_mutex_lock(&loadBoat);
+	printf("Adult %s arrived in MOL\n", (char*) args);
+	fflush(stdout);
+	inOahu = false;
+	adultsInOahu--;
+	adultsInMol++;
+	pthread_mutex_unlock(&loadBoat);
+	pthread_cond_signal(&boatInMol);
+	return (void*) 0;
 }
 
 void initSynch(){
+
 	pthread_mutex_init(&loadBoat, NULL);
 	pthread_mutex_init(&inOahuChildLock, NULL);
 	pthread_mutex_init(&inMolChildLock, NULL);
 	pthread_mutex_init(&inOahuAdultLock, NULL);
 	pthread_mutex_init(&inMolAdultLock, NULL);
-  	pthread_cond_init (&boatFull, NULL);
+  	pthread_cond_init (&boatInOahu, NULL);
+  	pthread_cond_init (&boatInMol, NULL);
+  	pthread_cond_init (&lastAdultCrossed, NULL);
   	pthread_cond_init (&showedUpInOahu, NULL);
   	pthread_cond_init (&finishedTransporting, NULL);
   	pthread_cond_init (&transportingChildren, NULL);
-  	numpeople = sem_open("numpeople", O_CREAT|O_EXCL, 0466, numadults + numchildren);
-  	while (numpeople==SEM_FAILED) {
-	    if (errno == EEXIST) {
-	      printf("semaphore numpeople already exists, unlinking and reopening\n");
-	      fflush(stdout);
-	      sem_unlink("numpeople");
-	      numpeople = sem_open("numpeople", O_CREAT|O_EXCL, 0466, 0);
-	    }
-	    else {
-	      printf("semaphore could not be opened, error # %d\n", errno);
-	      fflush(stdout);
-	      exit(1);
-	    }
-  	}
+  	
   	
 
 
